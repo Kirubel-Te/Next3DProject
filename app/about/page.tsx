e409ac401a8c9022f649d04415313c62c25eb096
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2026-02-05 00:06:29 +0300

    about page finished

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -51,6 +51,14 @@ const page = () => {
         </div>
         <hr className='font-extralight border-t border-gray-600/60'/>
         <br/>
+        <div className='w-[48%] mx-auto mb-12 mt-10'>
+            <h1 className='text-3xl sm:text-4xl md:text-5xl font-bold montserrat-alternates-bold mb-4 md:mb-7'>Our Vision</h1>
+            <p className='text-sm sm:text-base md:text-lg'>At PrintForge, we believe that 3D printing is revolutionizing the way we create, prototype, and manufacture. Our platform serves as a bridge between designers and makers, enabling the sharing of knowledge and creativity that pushes the boundaries of what's possible with 3D printing</p>
+            <br/>
+            <hr className='w-[40%] mx-auto'/>
+            <br/>
+            <p className='text-sm sm:text-base md:text-lg'>Whether you're a hobbyist looking for your next weekend project, an educator seeking teaching materials, or a professional designer wanting to share your creations, PrintForge provides the tools and community to support your journey in 3D printing.</p>
+        </div>
     </section>
     
   )
