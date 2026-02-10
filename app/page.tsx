f9b3718784bdf31501a6c128c2d9c8c26092199f
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2026-02-10 22:42:21 +0300

    button link added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="relative grid md:grid-cols-2 grid-cols-1 gap-5 w-[80%] mt-12 mb-5 mx-auto">
@@ -6,7 +7,7 @@ export default function Home() {
         <p className="mb-5 uppercase">Your go-to platform for 3D printing files</p>
         <h1 className="text-6xl font-bold mb-8">Discover what's possible with 3D printing</h1>
         <p className="text-xl mb-8">Join our community of creators and explore a vast library of user-submitted models.</p>
-        <button className="border-2 px-5 py-2 font-semibold cursor-pointer">Browse Models</button>
+        <Link href="/3d-models"><button className="border-2 px-5 py-2 font-semibold cursor-pointer">Browse Models</button></Link>
       </div>
       <div className="pt-12 px-3 flex justify-center">
         <div className="relative">
